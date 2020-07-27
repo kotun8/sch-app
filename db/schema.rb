a6622cfe500e73874e7b2f7a8522121da4da34bc
kotun08 <mkotun@gmail.com>
2020-07-27 16:38:17 +0100

    add sessions

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_25_074727) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "subjects", force: :cascade do |t|
     t.string "title"
     t.string "teacher"
